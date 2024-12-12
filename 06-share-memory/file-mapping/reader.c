bdb2aa124cd29ced9e4dd1fd947244a1368244c2
Đường Long <147648460+longruoi2k4@users.noreply.github.com>
2024-12-12 11:50:26 +0700

    update

---
+++ 06-share-memory/file-mapping/reader.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define SHM_SIZE 200
+#define FILE_NAME "test_mmap"
+
+int main() {
+
+    /*Step 1: Create fd*/
+    int shm_fd = shm_open(FILE_NAME, O_CREAT | O_RDWR, 0666);
+    if (shm_fd < 0) {
+        printf("shm_open() failed, %s.\n", strerror(errno));
+        return -1;
+    }
+
+    /*Step 2: Set size */
+    ftruncate(shm_fd, SHM_SIZE);
+
+    /*Step 3: Mapping*/
+    char *data = (char *)mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    printf("Data: %s.\n", data);
+
+    munmap(data, SHM_SIZE);
+
+    /*Step 4: Delete file mapping*/
+      // close(shm_fd);
+    shm_unlink("test_mmap");
+    return 0;
+}
